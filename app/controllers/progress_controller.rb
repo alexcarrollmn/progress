9a6676e38336eee2a26d0fe531d7a6886c3d9953
Brad Olson <alllateralus@gmail.com>
2011-05-12 16:02:56 -0500

    basic formatting.

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -2,9 +2,8 @@ class ProgressController < ApplicationController
 
   def index
     @img = ImageHandle.new('public/images/capture/', 'jpeg')
-    @img.recent_offset(2)
-    @img.listall
     @foo = @img.multi_offset(2,4)
+    @bar = @img.listall
   end
 
   def short
--- app/models/image_handle.rb
+++ app/models/image_handle.rb
@@ -4,28 +4,30 @@ class ImageHandle
     # gets all files in specified directory, orders them by create date.
     @files = Dir.glob("#{matching}*.#{filetype}").sort_by{|c| File.stat(c).ctime}
   end
+
   def listall
     total_list = Array.new
     @files.each do |file|
-      puts file
       total_list << file
     end
     total_list
   end
+  
   def most_recent
     @files.last
   end
+
   def recent_offset(offset = 0)
     num = @files.length < offset ? 0 : @files.length - (offset + 1)
     @files[num]
   end
+
   def multi_offset(amount, offset = 0)
     image_list = Array.new
     for part in (1..amount)
       if(part < @files.length)
-        image_list << recent_offset(offset)
+        image_list << recent_offset(offset * part)
       end
-      offset += offset
     end
     image_list
   end
