5dbb8d65bbac32e30650988df26795985c1799ff
Brad Olson <alllateralus@gmail.com>
2011-05-12 19:25:31 -0500

    fixed calling issue in frontend

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -1,11 +1,11 @@
 class ProgressController < ApplicationController
   def index
-    @img = ImageHandle.new('public/images/capture/', 'jpeg')
-    @foo = @img.multi_offset(2,4)
+    img = ImageHandle.new('public/images/capture/', 'jpeg')
+    @foo = img.multi_offset(2,4)
     
-    @bar = @img.listall
+    @bar = img.listall
 
-    image_compare('public/images/capture/', @img.most_recent, @img.recent_offset(10)).each do |hi|
+    image_compare(img.most_recent, img.recent_offset(10)).each do |hi|
       puts hi.to_s + "foo" 
     end
   end
@@ -19,7 +19,7 @@ class ProgressController < ApplicationController
   def day
   end
 
-  def image_compare(dir, image_base, image_compare)
+  def image_compare(image_base, image_compare)
     base_img = Magick::Image.read("#{image_base}").first
     current_img = Magick::Image.read("#{image_compare}").first
 
